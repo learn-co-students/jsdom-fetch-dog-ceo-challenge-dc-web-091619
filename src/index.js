1ade7396df0966e7a17a368d3514dbfc52eee749
Antonio Abraham <tony.calablava@gmail.com>
2019-10-30 16:46:45 -0400

    MVP done,all  deliverables met"

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(response => response.json())
+        .then (json => {
+            json.message.forEach((animal)=>{
+                renderAnimal(animal)
+            })
+
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then (json => {
+
+           for(const breed in json.message) {
+               renderBreed(breed)
+           }
+        })
+        })
+    const dropdown = document.querySelector('#breed-dropdown')
+    const selectValue = document.querySelector('#breed-dropdown').value
+    dropdown.addEventListener('change', filterBreed)
+})
+
+function renderAnimal (animal){
+    let dogContainer = document.querySelector('#dog-image-container')
+    let animalImg = document.createElement('img')
+    dogContainer.appendChild(animalImg)
+    animalImg.src = animal
+}
+
+function renderBreed(breed) {
+    let dogBreed = document.querySelector('#dog-breeds')
+    let breedItem = document.createElement("li")
+    breedItem.innerText = breed
+    dogBreed.appendChild(breedItem)
+    breedItem.addEventListener('click', handleClickEvent)
+
+}
+
+function handleClickEvent(event) {
+    event.target.style.color = "red"
+}
+
+function filterBreed(event) {
+   document.querySelector('#dog-breeds').innerHTML = ""
+    let targetValue = event.target.value
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then(json => {
+            for(const breed in json.message){
+
+                if(targetValue === breed[0]){
+                    renderBreed(breed)
+                }
+            }
+        })
+
+}
